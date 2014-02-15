e7459a8f20651552f66453ef0ed7c6687ab08d92
Matt Reilly <kb1vc@kb1vc.org>
2014-02-15 03:05:28 +0000

    Initial commit from kb1vc home svn 209

---
+++ src/CWGenerator.cxx
@@ -0,0 +1,298 @@
+/*
+Copyright (c) 2012, Matthew H. Reilly (kb1vc)
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
+
+    Redistributions of source code must retain the above copyright
+    notice, this list of conditions and the following disclaimer.
+    Redistributions in binary form must reproduce the above copyright
+    notice, this list of conditions and the following disclaimer in
+    the documentation and/or other materials provided with the
+    distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include "CWGenerator.hxx"
+#include <ctype.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+using namespace SoDa;
+
+std::map<char, std::string> CWGenerator::morse_map; 
+
+CWGenerator::CWGenerator(DatMBox * cw_env_stream, double _samp_rate, unsigned int _env_buf_len)
+{
+  env_stream = cw_env_stream;
+  sample_rate = _samp_rate;
+  env_buf_len = _env_buf_len; 
+
+  if(morse_map.empty()) initMorseMap();
+
+  // we want a rising/trailing edge attack that
+  // has a 5mS rise/fall time.
+  // Thats
+  edge_sample_count = (int) round(0.005 * sample_rate);
+
+  rising_edge = new float[edge_sample_count];
+  falling_edge = new float[edge_sample_count];
+  
+  // make a soft rising edge
+  // and a soft falling edge
+  float ang = 0.0;
+  float ang_incr = M_PI / ((float) edge_sample_count); 
+  for(unsigned int i = 0; i < edge_sample_count; i++) {
+    rising_edge[i] = 0.5 * (1.0 - cos(ang));
+    falling_edge[i] = 0.5 * (1.0 + cos(ang));
+    ang += ang_incr; 
+  }
+
+  // now allocate the worst case dit and dah buffers.
+  unsigned int dit_samples = (unsigned int) (round(sample_rate * 2.6)); 
+  dit = new float[dit_samples];  // "dit is mark + space"
+  dah = new float[dit_samples * 2]; // "dah is mark mark mark + space"
+  inter_word_space = new float[dit_samples * 3];
+  inter_char_space = inter_word_space; 
+
+  // and set a default speed
+  setCWSpeed(10);
+
+  // allocate our first outbound buffer
+  cur_buf = getFreeSoDaBuf();
+  cur_buf_idx = 0; 
+  // we really want a buffer that is the complex length. 
+  cur_buf_len = cur_buf->getComplexMaxLen(); 
+
+  // how big is this buffer, and how many of them do I need for 1 second of TX time.
+  unsigned int blen = cur_buf->getComplexMaxLen();
+  bufs_per_sec = (unsigned int) (sample_rate / ((double) blen));
+
+  // we aren't in the middle of a digraph right now. 
+  in_digraph = false; 
+}
+
+static bool toggle = false; 
+bool CWGenerator::readyForMore()
+{
+  // if we've got less than 1 second's worth of
+  // elements buffered up, then return true;
+  unsigned int ifc = env_stream->inFlightCount();
+
+  return (ifc < bufs_per_sec); 
+}
+
+void CWGenerator::initMorseMap()
+{
+  morse_map['a'] = ".-";
+  morse_map['b'] = "-...";
+  morse_map['c'] = "-.-.";
+  morse_map['d'] = "-..";
+  morse_map['e'] = ".";
+  morse_map['f'] = "..-.";
+  morse_map['g'] = "--.";
+  morse_map['h'] = "....";
+  morse_map['i'] = "..";
+  morse_map['j'] = ".---";
+  morse_map['k'] = "-.-";
+  morse_map['l'] = ".-..";
+  morse_map['m'] = "--";
+  morse_map['n'] = "-.";
+  morse_map['o'] = "---";
+  morse_map['p'] = ".--.";
+  morse_map['q'] = "--.-";
+  morse_map['r'] = ".-.";
+  morse_map['s'] = "...";
+  morse_map['t'] = "-";
+  morse_map['u'] = "..-";
+  morse_map['v'] = "...-";
+  morse_map['w'] = ".--";
+  morse_map['x'] = "-..-";
+  morse_map['y'] = "--.-";
+  morse_map['z'] = "--..";
+  morse_map['0'] = "-----";
+  morse_map['1'] = ".----";
+  morse_map['2'] = "..---";
+  morse_map['3'] = "...--";
+  morse_map['4'] = "....-";
+  morse_map['5'] = ".....";
+  morse_map['6'] = "-....";
+  morse_map['7'] = "--...";
+  morse_map['8'] = "---..";
+  morse_map['9'] = "----.";
+  morse_map['.'] = ".-.-.-";
+  morse_map[','] = "--..--";
+  morse_map['?'] = "..--..";
+  morse_map['-'] = "-...-";
+  morse_map['/'] = "-..-.";
+}
+
+void CWGenerator::setCWSpeed(unsigned int wpm)
+{
+  if(wpm < 1) wpm = 1;
+  if(wpm > 50) wpm = 50; 
+  words_per_minute = wpm;
+
+  float dot_time_s = 1.20 / ((float) wpm);
+  int dot_samples = (int) round(sample_rate * dot_time_s);
+  int dah_samples = dot_samples * 3; 
+
+  // now create the dit, dah, and space pattern.
+  int i, j;
+  for(i = 0; i < edge_sample_count; i++) {
+    dit[i] = rising_edge[i]; 
+  }
+  for(; i < (dot_samples - edge_sample_count); i++) {
+    dit[i] = 1.0; 
+  }
+  for(j = 0; i < dot_samples; i++, j++) {
+    dit[i] = falling_edge[j]; 
+  }
+  for(; i < dot_samples * 2; i++) {
+    dit[i] = 0.0; 
+  }
+  dit_len = i; 
+
+  for(i = 0; i < edge_sample_count; i++) {
+    dah[i] = rising_edge[i]; 
+  }
+  for(; i < (dah_samples - edge_sample_count); i++) {
+    dah[i] = 1.0; 
+  }
+  for(j = 0; i < dah_samples; i++, j++) {
+    dah[i] = falling_edge[j]; 
+  }
+  for(; i < dah_samples + dot_samples; i++) {
+    dah[i] = 0.0; 
+  }
+  dah_len = i;
+
+  ics_len = dit_len;
+  iws_len = dit_len * 3;
+  first_iws_len = iws_len - (ics_len / 2); 
+
+  for(i = 0; i < iws_len; i++) {
+    inter_word_space[i] = 0.0; 
+  }
+}
+
+void CWGenerator::appendToOut(const float * v, unsigned int vlen)
+{
+  int i;
+  int svlen = vlen; 
+  while(svlen != 0) {
+    int left = cur_buf_len - (cur_buf_idx + 1);
+    float *dbuf = cur_buf->getFloatBuf();
+
+    if(svlen <= left) {
+      memcpy(&(dbuf[cur_buf_idx]), v, svlen * sizeof(float));
+      cur_buf_idx += svlen;
+      svlen = 0; 
+    }
+    else {
+      // fill up the remainder of this buffer.
+      int remlen = cur_buf_len - cur_buf_idx;
+      memcpy(&(dbuf[cur_buf_idx]), v, remlen * sizeof(float));
+      v += remlen;
+      svlen -= remlen;
+      cur_buf_idx += remlen; 
+    }
+  
+    if(cur_buf_idx >= cur_buf_len) {
+      // post the buffer.
+      env_stream->put(cur_buf);
+      cur_buf = getFreeSoDaBuf(); 
+      cur_buf_idx = 0;
+      cur_buf_len = cur_buf->getComplexMaxLen(); 
+    }
+  }
+}
+
+void CWGenerator::flushBuffer()
+{
+  if(cur_buf_idx > 0) {
+    float *dbuf = cur_buf->getFloatBuf();
+    for(; cur_buf_idx < cur_buf_len; cur_buf_idx++) {
+      // fill the rest with zeros
+      dbuf[cur_buf_idx] = 0.0; 
+    }
+    // post the buffer.
+    env_stream->put(cur_buf);
+    cur_buf = getFreeSoDaBuf();
+    cur_buf_idx = 0;
+    cur_buf_len = cur_buf->getComplexMaxLen(); 
+  }
+}
+
+void CWGenerator::clearBuffer()
+{
+  if(cur_buf != NULL) {
+    cur_buf_idx = 0;
+    cur_buf_len = cur_buf->getComplexMaxLen();
+    cur_buf->setFloatLen(cur_buf_len); 
+  }
+}
+
+// returns true if the operation resulted in
+// something that took time... (an actual element)
+bool CWGenerator::sendChar(char c)
+{
+  c = tolower(c);
+  if(c == '_') {
+    in_digraph = true;
+    return false; 
+  }
+
+  if(c == ' ') {
+    in_digraph = false;
+    if(last_was_space) {
+      appendToOut(inter_word_space, iws_len);       
+    }
+    else {
+      appendToOut(inter_word_space, first_iws_len);       
+    }
+    last_was_space = true;
+    return true; 
+  }
+  else {
+    last_was_space = false;
+  }
+
+  if(morse_map.find(c) == morse_map.end()) return false;
+  
+  std::string symb = morse_map[c];
+
+  for(std::string::iterator si = symb.begin();
+      si != symb.end();
+      ++si) {
+    if(*si == '.') {
+      appendToOut(dit, dit_len); 
+    }
+    else if(*si == '-') {
+      appendToOut(dah, dah_len); 
+    }
+  }
+  if(!in_digraph) appendToOut(inter_char_space, ics_len);
+  else in_digraph = false; 
+  
+  return true; 
+}
+
+
+
---
+++ src/USRPCtrl.hxx
@@ -0,0 +1,179 @@
+/*
+Copyright (c) 2012, Matthew H. Reilly (kb1vc)
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
+
+    Redistributions of source code must retain the above copyright
+    notice, this list of conditions and the following disclaimer.
+    Redistributions in binary form must reproduce the above copyright
+    notice, this list of conditions and the following disclaimer in
+    the documentation and/or other materials provided with the
+    distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+ ///
+ ///  @file USRPCtrl.hxx
+ ///  @brief Thread class that owns the USRP control channel and functions
+ ///
+ ///
+ ///  @author M. H. Reilly (kb1vc)
+ ///  @date   July 2013
+ ///
+
+#ifndef USRPCTRL_HDR
+#define USRPCTRL_HDR
+#include "SoDaBase.hxx"
+#include "MultiMBox.hxx"
+#include "Command.hxx"
+#include "Params.hxx"
+#include <uhd/utils/thread_priority.hpp>
+#include <uhd/utils/safe_main.hpp>
+#include <uhd/usrp/multi_usrp.hpp>
+#include <uhd/usrp/dboard_base.hpp>
+#include <uhd/types/tune_request.hpp>
+#include <uhd/types/tune_result.hpp>
+
+namespace SoDa {
+  ///  @class USRPCtrl
+  /// 
+  ///  Though libuhd is designed to be re-entrant, there are some indications
+  ///  that all control functions (set_freq, gain, and other operations)
+  ///  should originate from a single thread.  USRPCtrl does that.
+  ///
+  ///  USRPCtrl listens on the Command Stream message channel for
+  ///  requests from other components (including the SoDa::UI listener)
+  ///  and dumps status and completion reports back onto
+  ///  the command stream channel. 
+  class USRPCtrl : public SoDaThread {
+  public:
+    /// Constructor
+    /// Build a USRPCtrl thread
+    /// @param params Pointer to a parameter object with all the initial settings
+    /// and identification for the attached USRP
+    /// @param _cmd_stream Pointer to the command stream message channel.
+    USRPCtrl(Params * params, CmdMBox * _cmd_stream);
+    /// start the thread
+    void run();
+
+    /// return a pointer to the multi_usrp object -- used by
+    /// RX and TX processes to find the associated USRP widget.
+    /// @return a pointer to the USRP radio object
+    uhd::usrp::multi_usrp::sptr getUSRP() { return usrp; }
+  private:
+    Params * params; 
+
+    /// Parse an incoming command and dispatch.
+    /// @param cmd a command record
+    void execCommand(Command * cmd);
+    /// Dispatch an incoming GET command
+    /// @param cmd a command record
+    void execGetCommand(Command * cmd); 
+    /// Dispatch an incoming SET command
+    /// @param cmd a command record
+    void execSetCommand(Command * cmd); 
+    /// Dispatch an incoming REPort command
+    /// @param cmd a command record
+    void execRepCommand(Command * cmd); 
+
+    /// get the number of seconds since the "Epoch"
+    /// @return relative time in seconds
+    double getTime();
+
+    /// is the identified (rx or tx) front-end LO locked?
+    /// If not, set the tuning frequency to "the right thing"
+    /// @param req the requested frequency (and tuning discipline)
+    /// @param sel 'r' for RX LO, 't' for TX LO
+    /// @param cur tuning result, if the LO was locked. 
+    uhd::tune_result_t checkLock(uhd::tune_request_t & req,
+				 char sel,
+				 uhd::tune_result_t & cur);
+
+
+    /// Set the front-end (LO + DDS) frequency to 'freq'
+    /// This includes setting the PLL front end synthesizer
+    /// as well as the FPGA resident digital synthesizer.
+    /// @param freq target frequency (LO and DDS combined)
+    /// @param sel 'r' for RX LO, 't' for TX LO
+    /// @param set_if_freq if TRUE, tell the USRPRX thread to reset
+    /// its front-end frequency so that it can adjust its own oscillator.
+    void set1stLOFreq(double freq, char sel, bool set_if_freq = false);
+
+    
+    CmdMBox * cmd_stream; ///< command stream channel
+    unsigned int subid;   ///< subscriber ID for this thread's connection to the command channel
+
+    // USRP stuff.
+    uhd::usrp::multi_usrp::sptr usrp; ///< to which USRP unit is this connected?
+    uhd::usrp::dboard_iface::sptr dboard;  ///< the daughterboard we're controlling
+
+    // parallel IO to turn on RX ena and TX ena
+    // specific to WBX right now.
+
+    /// Initialize the GPIO control registers to set the
+    /// direction and enables for the TX/RX relay output and sense input
+    void initControlGPIO();
+
+    /// get the state of the TXEna bit
+    /// @return true if the TX relay is activated. 
+    bool getTXEna();
+    /// get the state of the TX relay confirm bit
+    /// @return true if the TX relay sense input is asserted
+    bool getTXRelayOn(); 
+    /// turn TX on/off
+    /// @param val true to enable the transmitter, false otherwise.
+    void setTXEna(bool val);
+
+    /// we use TX_IO bit 12 to turn on the TX relay
+    /// we use TX_IO bit 11 to monitor the TX relay
+    static const unsigned int TX_RELAY_CTL; ///< mask for RELAY control bit
+    static const unsigned int TX_RELAY_MON; ///< mask for RELAY sense bit
+    
+    uhd::tune_result_t last_rx_tune_result; ///< RX tune result -- actual LO and DSP freq
+    uhd::tune_result_t last_tx_tune_result; ///< TX tune result
+    uhd::tune_result_t saved_rx_tune_result; ///< previous RX tune result -- used for transverter LO "calibration" function
+
+    double first_gettime; ///< timestamps are relative to the first timestamp.
+
+    // gain settings
+    double rx_rf_gain; ///< rf gain for RX front end amp/attenuator
+    double tx_rf_gain; ///< rf gain for final TX amp
+
+    // state of the box
+    bool tx_on; ///< if true, we are transmitting.
+
+    double last_rx_req_freq;  ///< remember the last setting -- useful for "calibration check" 
+    
+    // we horse the TX tuning around when we switch to RX
+    // to move the transmit birdie out of band.
+    double tx_freq; ///< remember current tx freq 
+    double tx_freq_rxmode_offset; ///< when in RX mode, move tx off frequency to put the tx birdie out of band, when in TX mode, this is 0
+    static const double rxmode_offset = 1e6; ///< tx offset when in RX mode
+
+    double tx_samp_rate; ///< sample rate to USRP TX chain. 
+    std::string tx_ant;  ///< TX antenna choice (usually has to be TX or TX/RX1?
+
+    // need this for TX/RX enable.
+    uhd::property_tree::sptr tx_fe_subtree; ///< property tree from daughtercard module
+
+    // enables verbose messages
+    bool debug_mode; ///< print stuff when we are in debug mode
+  };
+}
+
+
+#endif
