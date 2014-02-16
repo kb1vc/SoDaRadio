66fef88d87b137ca19781ba35406b47946738de0
Matt Reilly <kb1vc@kb1vc.org>
2014-02-16 13:44:35 +0000

    create a branch for working on the OSX port

---
+++ branches/OSXport/src/AudioALSA.cxx
@@ -0,0 +1,216 @@
+/*
+  Copyright (c) 2012, Matthew H. Reilly (kb1vc)
+  All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are
+  met:
+
+  Redistributions of source code must retain the above copyright
+  notice, this list of conditions and the following disclaimer.
+  Redistributions in binary form must reproduce the above copyright
+  notice, this list of conditions and the following disclaimer in
+  the documentation and/or other materials provided with the
+  distribution.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+  HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include "AudioALSA.hxx"
+
+#include <alsa/asoundlib.h>
+#include <boost/format.hpp>
+
+namespace SoDa {
+#if HAVE_LIBASOUND
+  AudioALSA::AudioALSA(unsigned int _sample_rate,
+		       DataFormat _fmt,
+		       unsigned int _sample_count_hint) :
+    AudioIfc(_sample_rate, _fmt, _sample_count_hint, "AudioALSA ALSA Interface") {
+
+    // code is largely borrowed from equalarea.com/paul/alsa-audio.html
+    setupPlayback();
+
+    setupCapture();
+  }
+
+  void AudioALSA::setupPlayback()
+  {
+    // setup the playback (output) stream
+    char pcm_name[] = "default"; 
+    if(snd_pcm_open(&pcm_out, pcm_name, SND_PCM_STREAM_PLAYBACK, SND_PCM_NONBLOCK) < 0) {
+      std::cerr << boost::format("can't open Alsa PCM device for output ... Crap.\n");
+      exit(-1); 
+    }
+
+    setupParams(pcm_out, hw_out_params); 
+  }
+
+  void AudioALSA::setupCapture()
+  {
+    char pcm_cap_name[] = "default"; // "hw:0,2";
+    snd_pcm_stream_t instream  = SND_PCM_STREAM_CAPTURE; 
+    if(snd_pcm_open(&pcm_in, pcm_cap_name, instream, 0) < 0) {
+      std::cerr << boost::format("can't open Alsa PCM device for  input... Crap.\n");
+      exit(-1); 
+    }
+
+    setupParams(pcm_in, hw_in_params);
+  }
+
+  void AudioALSA::setupParams(snd_pcm_t * dev, snd_pcm_hw_params_t *  & hw_params_ptr)
+  {
+    int err;
+    snd_pcm_hw_params_t * hw_paramsp;
+    
+    snd_pcm_hw_params_alloca(&hw_paramsp);
+
+    hw_params_ptr = hw_paramsp;
+
+    checkStatus(snd_pcm_hw_params_any (dev, hw_paramsp), "setupParams init parm block", true);
+    
+    checkStatus(snd_pcm_hw_params_set_access (dev, hw_paramsp, SND_PCM_ACCESS_RW_INTERLEAVED),
+		"setupParams set access", true);
+
+    checkStatus(snd_pcm_hw_params_set_format (dev, hw_paramsp, translateFormat(format)),
+		"setupParams set format", true);
+	
+    checkStatus(snd_pcm_hw_params_set_rate_near (dev, hw_paramsp, &sample_rate, 0), 
+		"setupParams set sample rate", true);
+	
+    checkStatus(snd_pcm_hw_params_set_channels (dev, hw_paramsp, 1), 
+		"setupParams set number of channels", true);
+
+    checkStatus(snd_pcm_hw_params_set_buffer_size (dev, hw_paramsp,
+						   sample_count_hint * datatype_size), 
+		"setupParams set buffer size", true);
+
+    checkStatus(snd_pcm_hw_params (dev, hw_paramsp), 
+		"setupParams set parameter block", true);
+	
+    checkStatus(snd_pcm_prepare (dev), 
+		"setupParams prepare audio interface", true);
+  }
+
+
+  bool AudioALSA::recvBufferReady(unsigned int len)  {
+    snd_pcm_sframes_t sframes_ready = snd_pcm_avail(pcm_in);
+    if(sframes_ready == -EPIPE) {
+      // we got an under-run... just ignore it.
+      int err; 
+      if(err = snd_pcm_recover(pcm_in, sframes_ready, 1) < 0) {
+	checkStatus(err, "recvBufferReady got EPIPE, tried recovery", false);
+      }
+      sframes_ready = snd_pcm_avail(pcm_in);
+    }
+    
+    if(sframes_ready < 0) {
+      checkStatus(sframes_ready, "recvBufferReady", false);
+    }
+    return sframes_ready >= len; 
+  }
+
+  bool AudioALSA::sendBufferReady(unsigned int len)  {
+    snd_pcm_sframes_t sframes_ready = snd_pcm_avail(pcm_out);
+    if(sframes_ready == -EPIPE) {
+      // we got an under-run... just ignore it.
+      int err; 
+      if(err = snd_pcm_recover(pcm_out, sframes_ready, 1) < 0) {
+	checkStatus(err, "sendBufferReady got EPIPE, tried recovery", false);
+      }
+      sframes_ready = snd_pcm_avail(pcm_out);
+    }
+
+    checkStatus(sframes_ready, "sendBufferReady", false);
+
+    return sframes_ready >= len; 
+  }
+
+  int AudioALSA::send(void * buf, unsigned int len) {
+    int err;
+    int olen = len;
+
+    char * cbuf = (char *) buf; 
+    while(1) {
+      err = snd_pcm_writei(pcm_out, cbuf, len);
+      
+      if(err == len) return len; 
+      else if(err == -EAGAIN) continue;
+      else if(err == -EPIPE) {
+	// we got an under-run... just ignore it.
+	if(err = snd_pcm_recover(pcm_out, err, 1) < 0) {
+	checkStatus(err, "send got EPIPE, tried recovery", false);
+	}
+      }
+      else if(err < 0) {
+	checkStatus(err, "send", true);
+      }
+      else if(err != len) {
+	len -= err;
+	cbuf += (err * datatype_size);
+      }
+    }
+   
+    return olen; 
+  }
+
+  int AudioALSA::recv(void * buf, unsigned int len, bool block) {
+    int err;
+    int olen = len;
+
+    int v1, v2;
+
+    char * cbuf = (char *) buf; 
+    while(1) {
+      err = snd_pcm_readi(pcm_in, cbuf, len);
+
+      if(err == len) return len; 
+      else if(err == -EAGAIN) continue; 
+      else if(err < 0) {
+	checkStatus(err, "recv", true);
+      }
+      else if(err != len) {
+	len -= err;
+	cbuf += (err * datatype_size);
+      }
+    }
+
+    return olen; 
+  }
+
+  
+  snd_pcm_format_t AudioALSA::translateFormat(AudioIfc::DataFormat fmt) {
+    switch(fmt) {
+    case FLOAT: return SND_PCM_FORMAT_FLOAT;
+      break; 
+    case DFLOAT: return SND_PCM_FORMAT_FLOAT64;
+      break;
+    case INT32: return SND_PCM_FORMAT_S32;
+      break; 
+    case INT16: return SND_PCM_FORMAT_S16;
+      break; 
+    case INT8: return SND_PCM_FORMAT_S8;
+      break; 
+    }
+    return SND_PCM_FORMAT_S16_LE; 
+  }
+#else
+  AudioALSA::AudioALSA(unsigned int _sample_rate,
+		       DataFormat _fmt,
+		       unsigned int _sample_count_hint) :
+    AudioIfc(_sample_rate, _fmt, _sample_count_hint, "AudioALSA ALSA Interface")
+  {
+    throw SoDa::SoDaException("ALSA Sound Library is not enabled in this build version.");  
+  }
+#endif // HAVE_LIBASOUND
+}
---
+++ branches/OSXport/src/AudioALSA.hxx
@@ -0,0 +1,211 @@
+#ifndef ALSA_PCM_HDR
+#define ALSA_PCM_HDR
+
+/*
+  Copyright (c) 2012, Matthew H. Reilly (kb1vc)
+  All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are
+  met:
+
+  Redistributions of source code must retain the above copyright
+  notice, this list of conditions and the following disclaimer.
+  Redistributions in binary form must reproduce the above copyright
+  notice, this list of conditions and the following disclaimer in
+  the documentation and/or other materials provided with the
+  distribution.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+  HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include "SoDaBase.hxx"
+#include "AudioIfc.hxx"
+#if HAVE_LIBASOUND
+#  include <alsa/asoundlib.h>
+#  define ALSA_DEF
+#else
+#  define ALSA_DEF { throw SoDa::SoDaException("ALSA Sound Library is not enabled in this build version."); } 
+#endif
+#include <boost/format.hpp>
+#include <iostream>
+#include <stdexcept>
+
+namespace SoDa {
+  /**
+   * @class AudioALSA
+   *
+   * @brief the ALSA audio interface class
+   *
+   * AudioALSA implements the interface specified by AudioIfc.  It should
+   * be interchangable with other audio interface handlers.
+   *
+   * The first several generations of SoDa used a Port Audio interface, but
+   * the PA library tends to spew a lot of extraneous "informational" messages
+   * on the console.  Though it is quite simple, the noise that PA creates makes
+   * it not my (kb1vc) interface of choice.
+   *
+   * ALSA on the other hand is documented poorly, organized oddly, and
+   * nearly inscrutable. But it is fast, and doesn't make a lot of noise on the
+   * console. 
+   * 
+   */
+  class AudioALSA : public AudioIfc {
+  public:
+    /**
+     * constructor
+     * @param _sample_rate in Hz -- 48000 is a good choice
+     * @param _fmt -- the format of the data (FLOAT, DFLOAT, INT32, INT16, INT8)
+     * @param _sample_count_hint -- the size of the buffers passed to and from
+     *                              the audio device (in samples)
+     */
+    AudioALSA(unsigned int _sample_rate,
+	      AudioIfc::DataFormat _fmt,
+	      unsigned int _sample_count_hint = 1024);
+
+#if HAVE_LIBASOUND
+    ~AudioALSA() {
+      snd_pcm_close(pcm_out);
+    }
+    
+    /**
+     * send -- send a buffer to the audio output
+     * @param buf buffer of type described by the DataFormat selected at init
+     * @param len number of elements in the buffer to send
+     * @return number of elements transferred to the audio output
+     */
+    int send(void * buf, unsigned int len);
+
+    /**
+     * sendBufferReady -- is there enough space in the audio device
+     *                    send buffer for a call from send?
+     * @param len the number of samples that we wish to send
+     * @return true if there is sufficient space. 
+     */
+    bool sendBufferReady(unsigned int len);
+
+    /**
+     * recv -- get a buffer of data from the audio input
+     * @param buf buffer of type described by the DataFormat selected at init
+     * @param len number of elements in the buffer to get
+     * @param block make this a blocking call --- ignored. 
+     * @return number of elements transferred from the audio input
+     */
+    int recv(void * buf, unsigned int len, bool block = true); 
+
+    /**
+     * recvBufferReady -- is there enough space in the audio device
+     *                    recv buffer for a call from recv?
+     * @param len the number of samples that we wish to get
+     * @return true if there is sufficient space. 
+     */
+    bool recvBufferReady(unsigned int len);
+
+    /**
+     * stop the output stream so that we don't encounter a buffer underflow
+     * while the reciever is muted.
+     */
+    void sleepOut() {
+      snd_pcm_drain(pcm_out);
+    }
+    /**
+     * start the output stream
+     */
+    void wakeOut() {
+      int err; 
+      if((err = snd_pcm_prepare(pcm_out)) < 0) {
+	throw
+	  SoDaException((boost::format("AudioALSA::wakeOut() Failed to wake after sleepOut() -- %s")
+			 % snd_strerror(err)).str(), this);
+      }
+      if((err = snd_pcm_start(pcm_out)) < 0) {
+	throw
+	  SoDaException((boost::format("AudioALSA::wakeOut() Failed to wake after sleepOut() -- %s")
+			 % snd_strerror(err)).str(), this);
+      }
+    }
+        
+    /**
+     * stop the input stream so that we don't encounter a buffer overflow
+     * while the transmitter is inactive.
+     */
+    void sleepIn() {
+      snd_pcm_drop(pcm_in); 
+    }
+    /**
+     * start the input stream
+     */
+    void wakeIn() {
+      int err; 
+      if((err = snd_pcm_prepare(pcm_in)) < 0) {
+	throw
+	  SoDaException((boost::format("AudioALSA::wakeIn() Failed to wake after sleepIn() -- %s")
+			 % snd_strerror(err)).str(), this);
+      }
+      if((err = snd_pcm_start(pcm_in)) < 0) {
+	throw
+	  SoDaException((boost::format("AudioALSA::wakeIn() Failed to wake after sleepIn() -- %s")
+			 % snd_strerror(err)).str(), this);
+      }
+    }
+
+  protected:
+    
+    snd_pcm_t * pcm_out; ///< The playback (output) handle. 
+    snd_pcm_t * pcm_in;  ///< The capture (input) handle. 
+    snd_pcm_hw_params_t * hw_in_params;  ///< the input parameter list
+    snd_pcm_hw_params_t * hw_out_params; ///< the output parameter list
+    
+    /**
+     * setup the playback handle and features. 
+     */
+    void setupPlayback();
+
+    /**
+     * setup the capture handle and features.
+     */
+    void setupCapture(); 
+
+    /**
+     * setup the parameters for a PCM device
+     * @param dev the device handle
+     * @param hw_params (out parameter) a pointer to a device parameter block
+     */
+    void setupParams(snd_pcm_t * dev, snd_pcm_hw_params_t * & hw_params);
+    
+    /**
+     * ALSA has predefined data type codes corresponding to float/ints of various sizes.
+     * @param fmt the AudioIfc::DataFormat spec (FLOAT, DFLOAT, INT32, INT16, INT8)
+     * @return a format specifier from the ALSA PCM format list.
+     */
+    snd_pcm_format_t translateFormat(AudioIfc::DataFormat fmt);
+
+    /**
+     * checkStatus check to see if the return status from an alsa call was OK
+     * @param err -- the error number
+     * @param exp -- why are we here
+     * @param fatal -- if true, throw an exception, otherwise print an error to std::cerr
+     */
+    void checkStatus(int err, const std::string & exp, bool fatal = false) {
+      
+      if (err < 0) {
+	if(fatal) throw SoDaException((boost::format("%s %s") % exp % snd_strerror(err)).str(), this);
+	else std::cerr << boost::format("%s %s %s\n") % getObjName() % exp % snd_strerror(err);
+      }
+    }
+#endif // HAVE_LIBASOUND
+  };
+}
+
+
+#endif
