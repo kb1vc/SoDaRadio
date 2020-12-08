92a7f55f73d95def58e6b5ea2629800add11363d
Matt Reilly <kb1vc@kb1vc.org>
2020-12-08 11:43:08 -0500

    Added AudioQtRX,RXTX

---
+++ src/AudioQtRX.cxx
@@ -0,0 +1,76 @@
+/*
+  Copyright (c) 2012, Matthew H. Reilly (kb1vc)
+  All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are
+  met:
+
+  Redistributions of source code must retain the above copyright
+  notice, this list of conditions and the following disclaimer.
+  Redistributions in binary form must reproduce the above copyright
+  notice, this list of conditions and the following disclaimer in
+  the documentation and/or other materials provided with the
+  distribution.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+  HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include "Debug.hxx"
+#include "AudioQtRX.hxx"
+
+#include <boost/format.hpp>
+#define _USE_MATH_DEFINES
+#include <cmath>
+
+// Use the simple setup, as recent (March 2015) changes to the 
+// ALSA/pulseaudio interactions have made explicit selection
+// of parameters somewhat harder to figure out.  In particular, 
+// buffer management has become far more important.
+#define ALSA_USE_SIMPLE_SETUP
+
+namespace SoDa {
+  AudioQtRX::AudioQtRX(unsigned int _sample_rate,
+		   unsigned int _sample_count_hint, 
+		   std::string audio_sock_basename, 
+		   std::string audio_port_name) :
+    AudioIfc(_sample_rate, _sample_count_hint, "AudioQtRX Qt Interface") {
+
+    std::cerr << "Creating AudioQtRX\n";
+    
+    setupNetwork(audio_sock_basename); 
+
+    ang = 0.0; 
+    ang_incr = 2.0 * M_PI / 48.0; 
+  }
+
+  void AudioQtRX::setupNetwork(std::string audio_sock_basename) 
+  {
+    std::string sockname = audio_sock_basename + "_rxa";
+    audio_rx_socket = new SoDa::UD::ServerSocket(sockname);
+    audio_rx_socket->setDebug(true);
+  }
+
+
+  bool AudioQtRX::sendBufferReady(unsigned int len)  {
+    return true; 
+  }
+
+
+  int AudioQtRX::send(void * buf, unsigned int len, bool when_ready) {
+    int ret;
+    ret = audio_rx_socket->put(buf, len, false);
+    return ret; 
+  }
+
+}
---
+++ src/AudioQtRX.hxx
@@ -0,0 +1,190 @@
+#pragma once
+
+/*
+  Copyright (c) 2012, Matthew H. Reilly (kb1vc)
+  All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are
+  met:
+
+  Redistributions of source code must retain the above copyright
+  notice, this list of conditions and the following disclaimer.
+  Redistributions in binary form must reproduce the above copyright
+  notice, this list of conditions and the following disclaimer in
+  the documentation and/or other materials provided with the
+  distribution.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+  HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include "SoDaBase.hxx"
+#include "AudioIfc.hxx"
+#include "UDSockets.hxx"
+#include <string>
+#include <mutex>
+// Only works if we have ALSA
+#include <alsa/asoundlib.h>
+#include <boost/format.hpp>
+#include <iostream>
+#include <stdexcept>
+
+#include <tuple>
+
+namespace SoDa {
+  /**
+   * @class AudioQtRX
+   *
+   * @brief Qt audio interface class
+   *
+   * AudioQt implements the interface specified by AudioIfc.  It should
+   * be interchangable with other audio interface handlers.  When BasebandRX
+   * posts data to the audio interface, it is transmitted via a unix domain
+   * socket to the GUI where the audio output device is managed. 
+   *
+   * For now, the transmit side is via ALSA.  That will change in time. 
+   *
+   * The first several generations of SoDa used a Port Audio interface, but
+   * the PA library tends to spew a lot of extraneous "informational" messages
+   * on the console.  Though it is quite simple, the noise that PA creates makes
+   * it not my (kb1vc) interface of choice.
+   *
+   * ALSA on the other hand is documented poorly, organized oddly, and
+   * nearly inscrutable. But it is fast, and doesn't make a lot of noise on the
+   * console. 
+   *
+   * On the other hand, ALSA doesn't support asynchronous callbacks on the
+   * audio output channel.  Because of this it really really stinks 
+   * when it comes to dealing with real-time problems.  Yes there is a documented
+   * "register a callback" function, but here's a surprise: it isn't supported 
+   * in certain cases that are common for Fedora, Ubuntu, and others.  
+   *
+   * Qt, on the other hand, is well documented, nicely written, and 
+   * works pretty much all the time.  
+   * 
+   */
+  class AudioQtRX : public AudioIfc, public Debug {
+  public:
+    /**
+     * constructor
+     * @param _sample_rate in Hz  48000 is a good choice
+     * @param _sample_count_hint  the size of the buffers passed to and from
+     *                              the audio device (in samples)
+     * @param audio_sock_basename starting string for the unix-domain socket 
+     *                            that carries the audio stream from the SoDaServer (radio) process
+     * @param audio_port_name  which ALSA device are we connecting to?
+     */
+    AudioQtRX(unsigned int _sample_rate,
+	    unsigned int _sample_count_hint = 1024,
+	    std::string audio_sock_basename = std::string("soda_"),
+	    std::string audio_port_name = std::string("default"));
+
+    ~AudioQtRX() {
+      delete audio_rx_socket;
+    }
+    
+    /**
+     * send -- send a buffer to the audio output
+     * @param buf buffer of type described by the DataFormat selected at init
+     * @param len number of elements in the buffer to send
+     * @param when_ready if true, test with sendBufferReady and return 0 if not ready
+     * otherwise perform the send regardless.
+     * @return number of elements transferred to the audio output
+     */
+    int send(void * buf, unsigned int len, bool when_ready = false);
+
+    /**
+     * sendBufferReady -- is there enough space in the audio device
+     *                    send buffer for a call from send?
+     * @param len the number of samples that we wish to send
+     * @return true if there is sufficient space. 
+     */
+    bool sendBufferReady(unsigned int len);
+
+    /**
+     * recv -- get a buffer of data from the audio input -- Always returns a buffer
+     * of 0.0.  AudioQtRXTX over-rides this method. 
+     * 
+     * @param buf buffer of type described by the DataFormat selected at init
+     * @param len number of elements in the buffer to get
+     * @param when_ready if true, test with recvBufferReady and return 0 if not ready
+     * otherwise perform the recv regardless.
+     * @return number of elements transferred from the audio input
+     */
+    virtual int recv(void * buf, unsigned int len, bool when_ready = false) { 
+      std::ignore = buf;
+      std::ignore = when_ready;
+      float *bp = (float*) buf;
+      for(int i = 0; i < len; i++) { bp[i] = 0.0; }
+      return len; 
+    }
+
+    /**
+     * recvBufferReady -- are there samples waiting in the audio device?
+     * Always returns true. 
+     *                    
+     * @param len the number of samples that we wish to get
+     * @return true if len samples are waiting in in the device buffer
+     */
+    bool recvBufferReady(unsigned int len) { return true; }
+
+    /**
+     * stop the output stream so that we don't encounter a buffer underflow
+     * while the reciever is muted.
+     */
+    void sleepOut() {
+    }
+    /**
+     * start the output stream
+     */
+    void wakeOut() {
+    }
+        
+    /**
+     * stop the input stream so that we don't encounter a buffer overflow
+     * while the transmitter is inactive.
+     */
+    virtual void sleepIn() { }
+    /**
+     * start the input stream
+     */
+    virtual void wakeIn() { }
+
+    std::string currentPlaybackState() {
+      return std::string("Fabulous");
+    }
+
+    virtual std::string currentCaptureState() {
+      return std::string("NOT IMPLEMENTED");
+    }
+    
+  protected:
+    /**
+     * setup the network sockets for the audio link to the user interface.
+     */
+    void setupNetwork(std::string audio_sock_basename) ;
+    
+    
+
+  private:
+
+  private:
+    SoDa::UD::ServerSocket * audio_rx_socket; 
+
+    // debug assistance
+    float ang; 
+    float ang_incr; 
+  };
+
+}
+
